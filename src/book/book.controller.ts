90da7d2f80da8818cee7a50dbaa7e8d3ed32d39f
robiul@hasan <robihasan093@gmail.com>
2025-07-11 14:32:44 +0600

    implement pagination & search functionality

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -6,20 +6,23 @@ import {
   Param,
   Post,
   Put,
+  Query,
 } from '@nestjs/common';
 import { BookService } from './book.service';
 import { Book } from './schemas/book.schemas';
 import { CreateBookDto } from './dto/create-book.dto';
 import { handleError } from 'src/common/utils/error.util';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { Query as ExpressQuery } from 'express-serve-static-core';
 
 @Controller('books')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
   @Get()
-  async getAllBooks(): Promise<Book[]> {
-    return this.bookService.findAll();
+  async getAllBooks(@Query() query: ExpressQuery)//: Promise<Book[]> 
+  {
+    return this.bookService.findAll(query);
   }
 
   @Get('/:id')
@@ -51,8 +54,7 @@ export class BookController {
   }
 
   @Delete('/:id')
-  deleteBook(@Param('id') id: string)
-  {
+  deleteBook(@Param('id') id: string) {
     return this.bookService.delete(id);
   }
 }
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -5,6 +5,7 @@ import { Book } from './schemas/book.schemas';
 import { CreateBookDto } from './dto/create-book.dto';
 import { handleError } from 'src/common/utils/error.util';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { Query } from 'express-serve-static-core';
 
 @Injectable()
 export class BookService {
@@ -13,17 +14,42 @@ export class BookService {
     private bookModel: Model<Book>,
   ) {}
 
-  async findAll(): Promise<Book[]> {
-    const books = await this.bookModel.find();
+  async findAll(query: Query)//: Promise<Book[]> 
+  {
+    const itemPrePage = 4;
+    const page = Number(query.page) || 1;
+    const skip = (page - 1) * itemPrePage;
+    console.log(query, page, skip)
 
-    return books;
+    const search = query.searchTerm
+      ? {
+          $or: [
+            { title: { $regex: query.searchTerm, $options: 'i' } },
+            { description: { $regex: query.searchTerm, $options: 'i' } },
+            { author: { $regex: query.searchTerm, $options: 'i' } },
+            { genre: { $regex: query.searchTerm, $options: 'i' } },
+          ],
+        }
+      : {};
+
+    const books = await this.bookModel
+      .find({ ...search })
+      .limit(itemPrePage)
+      .skip(skip);
+      
+      return {
+        total: books.length,
+        page: page,
+        limit: itemPrePage,
+        books
+    };
   }
 
   async findOne(bookId: string | undefined): Promise<Book | undefined> {
     const book = await this.bookModel.findOne({ id: bookId });
 
-    if(!book){
-      throw new NotFoundException(`Book with id ${bookId} not found`)
+    if (!book) {
+      throw new NotFoundException(`Book with id ${bookId} not found`);
     }
 
     return book ?? undefined;
@@ -48,7 +74,7 @@ export class BookService {
   async update(id: string | undefined, updateBookDto: UpdateBookDto) {
     const updatedBook = await this.bookModel.updateOne({ id }, updateBookDto, {
       new: true,
-      runValidators: true
+      runValidators: true,
     });
 
     return updatedBook;
